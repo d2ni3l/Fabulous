1b176b389b4d073eb6fae72b95ec3787e38e8d15
d2ni3l <107326751+d2ni3l@users.noreply.github.com>
2022-07-10 02:03:58 +0200

    Add files via upload

---
+++ Fabulousweb/assets/js/main.js
@@ -0,0 +1,115 @@
+/*
+	Spectral by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body'),
+		$wrapper = $('#page-wrapper'),
+		$banner = $('#banner'),
+		$header = $('#header');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ null,      '480px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Mobile?
+		if (browser.mobile)
+			$body.addClass('is-mobile');
+		else {
+
+			breakpoints.on('>medium', function() {
+				$body.removeClass('is-mobile');
+			});
+
+			breakpoints.on('<=medium', function() {
+				$body.addClass('is-mobile');
+			});
+
+		}
+
+	// Scrolly.
+		$('.scrolly')
+			.scrolly({
+				speed: 1500,
+				offset: $header.outerHeight()
+			});
+
+	// Menu.
+		$('#menu')
+			.append('<a href="#menu" class="close"></a>')
+			.appendTo($body)
+			.panel({
+				delay: 500,
+				hideOnClick: true,
+				hideOnSwipe: true,
+				resetScroll: true,
+				resetForms: true,
+				side: 'right',
+				target: $body,
+				visibleClass: 'is-menu-visible'
+			});
+
+	// Header.
+		if ($banner.length > 0
+		&&	$header.hasClass('alt')) {
+
+			$window.on('resize', function() { $window.trigger('scroll'); });
+
+			$banner.scrollex({
+				bottom:		$header.outerHeight() + 1,
+				terminate:	function() { $header.removeClass('alt'); },
+				enter:		function() { $header.addClass('alt'); },
+				leave:		function() { $header.removeClass('alt'); }
+			});
+
+		}
+
+})(jQuery);
+
+
+const sliderElm = document.querySelector(".slider-container .slider");
+const btnLeft = document.querySelector(".slider-container .btn-left");
+const btnRight = document.querySelector(".slider-container .btn-right");
+
+const numberSliderBoxs = sliderElm.children.length;
+let idxCurrentSlide = 0;
+
+// Functions:
+function moveSlider() {
+  let leftMargin = (sliderElm.clientWidth / numberSliderBoxs) * idxCurrentSlide;
+  sliderElm.style.marginLeft = -leftMargin + "px";
+  console.log(sliderElm.clientWidth, leftMargin);
+}
+function moveLeft() {
+  if (idxCurrentSlide === 0) idxCurrentSlide = numberSliderBoxs - 1;
+  else idxCurrentSlide--;
+
+  moveSlider();
+}
+function moveRight() {
+  if (idxCurrentSlide === numberSliderBoxs - 1) idxCurrentSlide = 0;
+  else idxCurrentSlide++;
+
+  moveSlider();
+}
+
+// Event Listeners:
+btnLeft.addEventListener("click", moveLeft);
+btnRight.addEventListener("click", moveRight);
+window.addEventListener("resize", moveSlider);
